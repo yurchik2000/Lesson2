81ea2511ea55d28de53ebc6d553b220a39da2f8c
Galunko <d:\Mail>
2022-05-27 09:12:56 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $('.outdoor__slider-list').slick({
+        prevArrow: '<svg class="slider__arrow slider__arrow-left" width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M23.463 13.2122L15.29 20.1822L23.463 27.1522" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><circle cx="20.6143" cy="20.1821" r="19" stroke="#6E6E6E" stroke-width="2"/></svg>',
+        nextArrow: '<svg class="slider__arrow slider__arrow-right" width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18.1454 13.2122L26.3184 20.1822L18.1454 27.1522" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><circle r="19" transform="matrix(-1 0 0 1 20.9941 20.1821)" stroke="#6E6E6E" stroke-width="2"/></svg>',
+        fade: true,
+    })
+    
+})
